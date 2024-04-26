bce930c590d61a6130bc03a71df6e83224150eb0
laibaali25 <168167711+laibaali25@users.noreply.github.com>
2024-04-26 13:36:06 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,24 @@
 function longestSubstring(s) {
 
+    let maxLength = 0;
+    let start = 0;
+    let charIndexMap = {};
 
-    // Implementation of longestSubstring function
+    for (let end = 0; end < s.length; end++) {
+        if (s[end] in charIndexMap) {
+            // If the character is already in the map, move the start pointer to the next index of the repeated character
+            start = Math.max(start, charIndexMap[s[end]] + 1);
+        }
+        // Update the index of the current character
+        charIndexMap[s[end]] = end;
+        // Update the maximum length
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
 }
 
+// Example usage:
+const inputString = "abcabcbb";
+console.log(longestSubstring(inputString)); // Output should be 3
 module.exports = { longestSubstring };
