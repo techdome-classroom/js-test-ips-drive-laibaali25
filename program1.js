7c51195ff88eaaa2e65344442122cd9a1fcb3153
laibaali25 <168167711+laibaali25@users.noreply.github.com>
2024-04-26 13:35:51 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,8 +2,35 @@ function smallestMissingPositiveInteger(nums) {
 
 
     // Implement the function smallest_missing_positive_integer
+    const n= nums.length;
+    for(let  i = 0; i< n; i++)
+    {
+      while(nums[i] > 0 && nums[i] <=n && nums[nums[i] - 1] !== nums[i])
+      {
+               swap(nums, i ,nums[i] - 1);
+      }
+    }
+
+    for(let i=0; i< n; i++)
   
+    {
+      if(nums[i] !== i+1)
+      {
+        return i+1;
+            }
+    }
+    return n+1;
   }
+    function swap(nums, i, j)
+    {
+          const temp = nums[i];
+          nums[i]  = nums[j];
+          nums[j]= temp;
+    }
+  
+    const array =[3.4,-1,1];
+    const smallestMissing = smallestMissingPositiveInteger(array);
+    console.log('smallest Missing Positive int' , smallestMissing);
   
   module.exports = smallestMissingPositiveInteger;
 
